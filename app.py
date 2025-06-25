4fe3f2da96e93e3ef0e93ae153dc8bf62e2dd814
João Pedro Souza <joaopedro150308@gmail.com>
2025-06-24 21:12:19 -0300

    Incremented the selecionar_nova_atividade

--- app.py
+++ app.py
@@ -23,29 +23,43 @@ def logar_netescola(matricula, senha, wait):
     sleep(1)
     botao_logar.click()
 
-def acessar_sergoias(wait):
+def acessar_sergoias(wait, driver, original_handle):
     botao_link_sergoias = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='sc-bcXHqe fcTwxh']/a[1]")))
     sleep(1)
     botao_link_sergoias.click()
+    wait.until(EC.number_of_windows_to_be(2))
+    janelas = driver.window_handles
+    print(original_handle)
+    print(janelas)
+    for janela in janelas:
+        if janela != original_handle:
+            driver.switch_to.window(janela)
+            break
+    print(driver.current_window_handle)
 
 
 def selecionar_nova_atividade(wait, driver):
-    input()
-    perfil_buttons = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='nui-avatar-inner']")))
-    driver.execute_script('Window.scrolltTo(0, document.body.scrollHeight);')
+    # input("Aperte qualquer tecla")
+    perfil_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//span[@class='nui-avatar-text']")))
+    print(perfil_buttons)
     sleep(2)
-    proximas_atividades = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='mt-8']/ul//a[@class='nui-button-icon nui-button-curved nui-button-small nui-button-default flex w-[50px] items-center justify-center']")))
-    print(len(proximas_atividades))
+    driver.execute_script('window.scrollBy(0, 1000);')
+    sleep(2)
+    proxima_atividade = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='mt-8']/ul//a[@class='nui-button-icon nui-button-curved nui-button-small nui-button-default flex w-[50px] items-center justify-center']")))
+    print(proxima_atividade)
+    sleep(1)
+    proxima_atividade.click()
 
 
 # --- Executing ---
 matricula = str(input('Digite sua matricula do net escola: '))
 senha = str(input('Digite sua senha do net escola: '))
 from selenium_starting import driver, wait
+original_handle = driver.current_window_handle
 navegar_ate_o_site(driver)
 logar_netescola(matricula, senha, wait)
 sleep(5)
-acessar_sergoias(wait=wait)
-sleep(10)
+acessar_sergoias(wait, driver, original_handle)
+sleep(30)
 selecionar_nova_atividade(wait, driver)
 input()
